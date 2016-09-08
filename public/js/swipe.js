41d2f81196a76d88cc73970a3a2509ac5cfe99f2
Neel Kuila <neelk1123@gmail.com>
2016-09-07 21:33:01 -0400

    Working front end

--- public/js/swipe.js
+++ public/js/swipe.js
@@ -18,11 +18,20 @@ console.log(stack);
 stack.on('throwout', function (e) {
     console.log(e.target.innerText || e.target.textContent, 'has been thrown out of the stack to the', e.throwDirection == 1 ? 'right' : 'left', 'direction.');
 
+    if (e.throwDirection == 1){ //thrown right
+        e.target.classList.add('good');
+    } else {
+      e.target.classList.add('bad');
+    }
     e.target.classList.remove('in-deck');
 });
 
 stack.on('throwin', function (e) {
     console.log(e.target.innerText || e.target.textContent, 'has been thrown into the stack from the', e.throwDirection == 1 ? 'right' : 'left', 'direction.');
 
+    if (e.target.classList.contains('good')) {  e.target.classList.remove('good');  }
+    if (e.target.classList.contains('bad'))  {  e.target.classList.remove('bad');  }
     e.target.classList.add('in-deck');
 });
+
+stack.on()
