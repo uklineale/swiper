7cc3a9b30b42a17bf35449400e9a6d9f62b3e9c9
root <wifu1234@gmail.com>
2016-09-16 15:12:42 +0000

    Configurable port for nginx. Ubuntu upstart file for daemonizing the node server.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     workers = require('./routes/workers'),
     path = require('path');
-
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,5 +17,5 @@ app.post('/workers', workers.addWorker);
 app.put('/workers/:id', workers.updateWorker);
 app.delete('/workers/:id', workers.deleteWorker); 
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.listen(port);
+console.log('Listening on port '+port+'...');
